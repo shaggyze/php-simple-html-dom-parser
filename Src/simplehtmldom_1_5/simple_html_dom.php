4c9297e9fc3af75f057c855c7c78a7814e8aa680
shaggyze <shaggyze@gmail.com>
2024-10-09 07:51:55 -0700

    Update simple_html_dom.php

--- Src/simplehtmldom_1_5/simple_html_dom.php
+++ Src/simplehtmldom_1_5/simple_html_dom.php
@@ -765,8 +765,12 @@ class simple_html_dom_node
 
             // convert to lowercase
             if ($this->dom->lowercase) {
-                $tag = strtolower($tag);
-                $key = strtolower($key);
+				if ($tag !== null) {
+					$tag = strtolower($tag);
+				}
+				if ($key !== null) {
+					$key = strtolower($key);
+				}
             }
             //elements that do NOT have the specified attribute
             if (isset($key[0]) && $key[0] === '!') {
